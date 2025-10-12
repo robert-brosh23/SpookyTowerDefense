6ee5241f0441b3e47d706920c164a36fc45cc647
robert.brosh23 <robert.brosh23@gmail.com>
2025-10-12 12:03:16 -0400

    create project, tile selection started

---
+++ main/main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const GRID_DIMENSIONS := [16, 20]
+
+@export var select_layer : TileMapLayer
+
+var prev_tile_selection_coords := Vector2(0,0)
+
+
+func _process(delta):
+	_process_input()
+	
+func _process_input():
+	select_layer.erase_cell(prev_tile_selection_coords)
+	
+	var select_tile_coords = select_layer.local_to_map(get_global_mouse_position())
+	prev_tile_selection_coords = select_tile_coords
+			
+	select_layer.set_cell(select_tile_coords, 4, Vector2(0,0), 0)
