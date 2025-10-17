06cd945bfcc7a652b6ebb092d320ead87acf9b44
robert.brosh23 <robert.brosh23@gmail.com>
2025-10-17 14:58:21 -0400

    turrets can be placed

--- main/main.gd
+++ main/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 const GRID_DIMENSIONS := [16, 20]
 
 @export var select_layer : TileMapLayer
+@export var base_layer : TileMapLayer
 
 var prev_tile_selection_coords := Vector2(0,0)
 
@@ -15,5 +16,7 @@ func _process_input():
 	
 	var select_tile_coords = select_layer.local_to_map(get_global_mouse_position())
 	prev_tile_selection_coords = select_tile_coords
-			
-	select_layer.set_cell(select_tile_coords, 1, Vector2(0,0))
+	
+	if base_layer.get_cell_source_id(select_tile_coords + Vector2i(0, 2)) == 2:
+		select_layer.set_cell(select_tile_coords, 1, Vector2(0,0))
+		
