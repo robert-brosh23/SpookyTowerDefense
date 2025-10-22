674ea54c9ff271f05e6aa459b55fe004ab2480b9
Greg <trahan.greg@gmail.com>
2025-10-21 21:50:17 -0500

    set turrets to auto-aim at nearest target

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -28,3 +28,4 @@ func destroy_enemy():
 	print("destroyed")
 	destroyed.emit()
 	queue_free()
+	
--- turret/bullet/bullet.gd
+++ turret/bullet/bullet.gd
@@ -5,6 +5,7 @@ const time_to_live = 4.0
 
 @export var shadow : Sprite2D
 @export var hitbox : Hitbox
+@export var detection_radius := 400.0
 
 var base_layer : TileMapLayer
 var direction := 0.0
@@ -18,7 +19,7 @@ static func spawn_bullet(bullet_direction: float, bullet_speed: float = DEFAULT_
 	return bullet
 
 func _physics_process(delta: float) -> void:
-	position += delta * speed * Vector2(cos(direction),sin(direction) * 0.5)
+	position += delta * speed * Vector2(cos(direction),sin(direction))
 	
 	if base_layer != null:
 		if base_layer.get_cell_source_id(base_layer.local_to_map(global_position + Vector2(0, 60))) == -1:
@@ -27,6 +28,11 @@ func _physics_process(delta: float) -> void:
 			shadow.visible = true
 
 func _ready() -> void:
+	# Aim towards nearest enemy
+	var target = _get_nearest_enemy()
+	if target:
+		direction = global_position.angle_to_point(target.hurtbox.global_position)
+
 	var timer = Timer.new()
 	base_layer = get_tree().get_first_node_in_group("base_layer")
 	add_child(timer)
@@ -34,3 +40,16 @@ func _ready() -> void:
 	timer.timeout.connect(func(): queue_free())
 	
 	hitbox.hit_connected.connect(func(): queue_free())
+	
+func _get_nearest_enemy() -> Enemy:
+	var enemies = get_tree().get_nodes_in_group("enemies")
+	var nearest : Enemy = null
+	var nearest_dist := INF
+	for e in enemies:
+		if not e or not e.is_inside_tree():
+			continue
+		var dist = global_position.distance_to(e.global_position)
+		if dist < nearest_dist and dist < detection_radius:
+			nearest = e
+			nearest_dist = dist
+	return nearest
